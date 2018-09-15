486a4b9e6cbc8ce8dc8d5c302db37b34eb9e2537
jidemobell <jidd32@gmail.com>
2018-09-15 04:04:55 +0100

    add eslint config files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser : "babel-eslint",
+  extends: ["airbnb"],
+  env : {
+    "es6" : true,
+    "browser": true
+  },
+  parserOptions: {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+        "jsx": true
+    },
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": 0,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "arrow-body-style":0,
+    "react/no-unescaped-entities":0,
+    "react/jsx-no-bind":0
+  }
+}
