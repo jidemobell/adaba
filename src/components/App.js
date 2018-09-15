f4c852cd0ab7918285b018cfdc383afd9ac1686b
jidemobell <jidd32@gmail.com>
2018-09-15 15:51:26 +0100

    add test content to App

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
       <div>
         <div className="container">
-          <h1>Under Maintenace</h1>
+          <h1>ADD YOUR SKILLS</h1>
           <p>Just messing around with data</p>
         </div>
       </div>
