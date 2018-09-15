4d901b1b58e4893ddcd5a66ca8688e9ee95b20d6
jidemobell <jidd32@gmail.com>
2018-09-15 08:37:44 +0100

    remove API check

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,6 @@ import React from 'react';
 
 
 class App extends React.Component {
-  componentDidMount() {
-    return fetch('http://localhost:3000/skills', {
-      method: 'GET',
-    })
-      .then(response => response.json())
-      .then((json) => {
-        console.log(json);
-      });
-  }
-
   render() {
     return (
       <div>
