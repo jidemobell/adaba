6f83cb9b31c84fde50e44f77038c872745df4ba0
jidemobell <jidd32@gmail.com>
2018-09-15 05:36:00 +0100

    fetch test data from API

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 
 class App extends React.Component {
+  componentDidMount() {
+    return fetch('http://localhost:3000/skills', {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((json) => {
+        console.log(json);
+      });
+  }
+
   render() {
     return (
       <div>
-        <h1>Under Maintenace</h1>
+        <div className="container">
+          <h1>Under Maintenace</h1>
+          <p>Just messing around with data</p>
+        </div>
       </div>
     );
   }
