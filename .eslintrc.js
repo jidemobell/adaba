e6a59a31fe4b8f551bc4ba9fc20dc78f39e47667
jidemobell <jidd32@gmail.com>
2018-09-15 04:43:41 +0100

    edit eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "arrow-body-style":0,
     "react/no-unescaped-entities":0,
-    "react/jsx-no-bind":0
+    "react/jsx-no-bind":0,
+    "react/prefer-stateless-function":0
   }
 }
