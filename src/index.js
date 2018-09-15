8ebaefb53ce3d9d75dced0271f0cca580e5ea689
jidemobell <jidd32@gmail.com>
2018-09-15 08:14:11 +0100

    craete app store and apply thunk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Provider from 'react-redux';
+import Thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
+import rootReducer from './reducers/rootReducer';
 
+const store = createStore(rootReducer, applyMiddleware(Thunk));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
