935f00a7e174211172ae2c26ef03c39001122f50
jidemobell <jidd32@gmail.com>
2018-09-16 15:00:46 +0100

    updatte skills form with redux form

--- src/components/SkillsForm/Form.js
+++ src/components/SkillsForm/Form.js
@@ -1,28 +1,42 @@
+// send options values and skills to state
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-class Form extends React.Component {
-  render() {
-    return (
-      <form className="form-controller">
-        <div className="form-item">
-          <input type="text" name="firstname" className="form-input" placeholder="Node.js, Postgres, React, etc.," />
-        </div>
-        <div className="form-item">
-          <select name="cars" className="select-input">
-            <option value=" " className="options  options-selected" selected>Experience</option>
-            <option value="< 1" className="options">{`<1 Year`}</option>
-            <option value="1 - 3" className="options">1 - 3 Years</option>
-            <option value="3 - 5" className="options">3 - 5 Years</option>
-            <option value="5 - 7" className="options">5 - 7Years</option>
-            <option value="7+" className="options">7+ Years</option>
-          </select>
-        </div>
-        <div className="form-item">
-          <input type="submit" value="Submit" className="form-button" />
-        </div>
-      </form>
-    );
-  }
-}
 
-export default Form;
+const skillForm = ({ handleSubmit }) => {
+  return (
+    <form className="form-controller" onSubmit={handleSubmit(val => console.log(val))}>
+      <div className="form-item">
+        <Field
+          type="input"
+          component="input"
+          name="name"
+          placeholder="Node.js, Postgres, React, etc.,"
+          className="form-input"
+        />
+      </div>
+      <div className="form-item">
+        <Field
+          type="input"
+          component="select"
+          name="experience"
+          placeholder="experience"
+          className="select-input"
+        >
+          <option value=" " className="options  options-selected">Experience</option>
+          <option value="< 1" className="options">{`<1 Year`}</option>
+          <option value="2 Years" className="options">1 - 3 Years</option>
+          <option value="4 Years" className="options">3 - 5 Years</option>
+          <option value="6 Years" className="options">5 - 7Years</option>
+          <option value="> 7 Years" className="options">7+ Years</option>
+        </Field>
+      </div>
+      <div className="form-item">
+        <input type="submit" value="Submit" className="form-button" />
+      </div>
+    </form>
+  );
+};
+
+
+export default reduxForm({ form: 'skillForm' })(skillForm);
