12bbac44d2b2617eef2bedccd7c6de839058fbfc
jidemobell <jidd32@gmail.com>
2018-09-16 16:11:56 +0100

    debug App state

--- src/components/SkillsPage/App.js
+++ src/components/SkillsPage/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import '../../styles/App.css';
 import '../../styles/response.css';
+
+import * as skillsActions from '../../actions/skills/skillsActions';
 
 import SkillCard from '../skillCard/SkillCard';
 import Form from '../SkillsForm/Form';
@@ -8,7 +12,12 @@ import Form from '../SkillsForm/Form';
 // skillCard to be added to skills div onSubmit
 // from a skillset array.
 
-class App extends React.Component {
+class SkillApp extends React.Component {
+  componentDidMount() {
+    const { actions } = this.props;
+    actions.getSkills();
+  }
+
   render() {
     return (
       <div>
@@ -28,5 +37,21 @@ class App extends React.Component {
     );
   }
 }
+
+const mapStatetToProps = (state) => {
+  console.log(state);
+  return {
+
+  };
+};
+
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(Object.assign(skillsActions), dispatch),
+  };
+};
+
+const App = connect(mapStatetToProps, mapDispatchToProps)(SkillApp);
 
 export default App;
