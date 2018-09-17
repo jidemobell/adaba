aeb17e6e349cef8826b9ed7f6fd8fc691be95c83
jidemobell <jidd32@gmail.com>
2018-09-17 08:03:11 +0100

    add new actions

--- src/actions/skills/skillsActions.js
+++ src/actions/skills/skillsActions.js
@@ -1,11 +1,26 @@
 import * as actionTypes from '../actionTypes';
 
-export function loadInfo(data) {
+export function getInfo(data) {
   return {
     type: actionTypes.SKILLS_FETCHED,
     payload: data,
   };
 }
+
+function loadInfo() {
+  return {
+    type: actionTypes.SKILL_POSTED,
+    payload: true,
+  };
+}
+
+function loadError() {
+  return {
+    type: actionTypes.SKILL_POSTED,
+    payload: false,
+  };
+}
+
 
 export function getSkills() {
   return (dispatch) => {
@@ -14,7 +29,29 @@ export function getSkills() {
     })
       .then(response => response.json())
       .then((json) => {
-        dispatch(loadInfo(json));
+        dispatch(getInfo(json));
       });
+  };
+}
+
+export function postSkills(val) {
+  return (dispatch) => {
+    const values = { name: val.name, expirience: val.expirience };
+    return fetch('http://localhost:3000/skills', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    })
+      .then(response => (response.json()))
+      .then(doc => dispatch(loadInfo()))
+      .catch(error => dispatch(loadError()));
+  };
+}
+
+export function resetPostState() {
+  return (dispatch) => {
+    dispatch(loadError());
   };
 }
