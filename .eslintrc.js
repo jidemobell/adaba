134b11ed01d057293058292bd106fa2125c34161
jidemobell <jidd32@gmail.com>
2018-09-17 08:01:36 +0100

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     "react/self-closing-comp":0,
     "quotes":0,
     "react/prop-types":0,
-    "object-curly-newline":0
+    "object-curly-newline":0,
+    "no-unused-vars":0
   }
 }
