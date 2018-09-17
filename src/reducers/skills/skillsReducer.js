ad67123989de12f16c4d87dc6f1d486214c01e7f
jidemobell <jidd32@gmail.com>
2018-09-17 08:07:22 +0100

    add POST_SKILL check to reducer

--- src/reducers/skills/skillsReducer.js
+++ src/reducers/skills/skillsReducer.js
@@ -4,6 +4,8 @@ const skillsReducer = (state = {}, action) => {
   switch (action.type) {
     case actionTypes.SKILLS_FETCHED:
       return { ...state, skills: action.payload };
+    case actionTypes.SKILL_POSTED:
+      return { ...state, postAction: action.payload };
     default:
       return state;
   }
