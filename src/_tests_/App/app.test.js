9c0dda60c6c7ad8df6a92530afd010b0d0c6c82b
jidemobell <jidd32@gmail.com>
2018-09-20 04:16:06 +0100

    add jest test folder

--- src/_tests_/App/app.test.js
+++ src/_tests_/App/app.test.js
@@ -1,32 +1,54 @@
+// /* eslint-disable no-undef */
+
+// import React from 'react';
+// import Enzyme, { mount } from 'enzyme';
+// import { Adapter } from 'enzyme-adapter-react-16';
+
+// import App from '../../containers/skillPage/App';
+
+// Enzyme.configure({ adapter: new Adapter() });
+
+// const setup = () => {
+//   const props = {
+//     skillsArray: jest.fn(),
+//   };
+
+//   const enzymeWrapper = mount(<App {...props} />);
+
+//   return {
+//     props,
+//     enzymeWrapper,
+//   };
+// };
+
+// describe('components', () => {
+//   describe('<App />', () => {
+//     it('should render self and components', () => {
+//       const { enzymeWrapper } = setup();
+
+//       expect(enzymeWrapper).find('div').hasClass('to-test').toBe(true);
+//     });
+//   });
+// });
+
 /* eslint-disable no-undef */
 
+
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
-import { Adapter } from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import { expect } from 'chai';
 
-import App from '../../containers/skillPage/App';
+import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+import App from '../../components/SkillsPage/App';
 
-const setup = () => {
-  const props = {
-    skillsArray: jest.fn(),
-  };
+configure({ adapter: new Adapter() });
 
-  const enzymeWrapper = mount(<App {...props} />);
-
-  return {
-    props,
-    enzymeWrapper,
-  };
-};
-
-describe('components', () => {
-  describe('<App />', () => {
-    it('should render self and components', () => {
-      const { enzymeWrapper } = setup();
-
-      expect(enzymeWrapper).find('div').hasClass('to-test').toBe(true);
-    });
+describe('App component testing', () => {
+  it('renders welcome message', () => {
+    const wrapper = shallow(<App />);
+    const welcome = <h1 className='App-title'>Welcome to React</h1>;
+    expect(wrapper.contains(welcome)).to.equal(true);
   });
 });
