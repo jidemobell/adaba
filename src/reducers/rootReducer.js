517dd73d508707713344ed738293046c24078dbd
jidemobell <jidd32@gmail.com>
2018-09-20 03:49:41 +0100

    update form reducer with plugin

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,21 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { ACCOUNT_SAVE_SUCCESS } from '../actions/actionTypes'; 
 import skillsReducer from './skills/skillsReducer';
 
 
 const rootReducer = combineReducers({
   stateData: skillsReducer,
-  form: formReducer,
+  form: formReducer.plugin({
+    account: (state, action) => { // <------ 'account' is name of form given to reduxForm()
+      switch (action.type) {
+        case ACCOUNT_SAVE_SUCCESS:
+          return action.payload;       // <--- blow away form data
+        default:
+          return state;
+      }
+    },
+  }),
 });
 
 export default rootReducer;
