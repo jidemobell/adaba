c074642431283987278b6d7181f358b3bd90fccb
jidemobell <jidd32@gmail.com>
2018-09-22 03:44:57 +0100

    updated select input option

--- src/container/SkillsForm/Form.js
+++ src/container/SkillsForm/Form.js
@@ -71,7 +71,7 @@ class RenderForm extends React.Component {
               <option value="2 Years" className="options">1 - 3 Years</option>
               <option value="4 Years" className="options">3 - 5 Years</option>
               <option value="6 Years" className="options">5 - 7Years</option>
-              <option value="> 7 Years" className="options">7+ Years</option>
+              <option value=">7 Years" className="options">7+ Years</option>
             </Field>
           </div>
           <div className="form-item">
